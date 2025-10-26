b65366108d438afb52b79822760e949a5e7f5f1a
slsl2 <kimminji7153@gmail.com>
2025-10-27 06:06:00 +0900

    feat(app): 이동 수 입력값 유효성 검사 함수 추가

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function fail(msg) {
 
 // 이름 입력 받는 함수
 async function inputCarNames() {
-  const NAMES_INPUT = await Console.readLineAsync(
+  const namesInput = await Console.readLineAsync(
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
   );
-  return NAMES_INPUT;
+  return namesInput;
 }
 
 // 이동 횟수 입력 받는 함수
 async function inputTotalRound() {
-  const TOTAL_ROUND_INPUT = await Console.readLineAsync(
+  const totalRoundInput = await Console.readLineAsync(
     "시도할 횟수는 몇 회인가요?\n"
   );
-  return TOTAL_ROUND_INPUT;
+  return totalRoundInput;
 }
 
 // 이름 유효성 검사
@@ -30,11 +30,26 @@ function validateCarName(name) {
 
 // 무작위 값 구해서 4 이상일 경우 전진 (true)
 function canMove() {
-  const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
-  if (RANDOM_NUMBER >= 4) {
+  const randomNumber = Random.pickNumberInRange(0, 9);
+  if (randomNumber >= 4) {
     return true;
   }
   return false;
+}
+
+// 이동 횟수 입력값 유효성 검사
+function validateTotalRound(totalRoundInput) {
+  if (totalRoundInput === "") {
+    fail("이동 횟수가 입력되지 않았습니다.");
+  }
+  const totalRound = Number(totalRoundInput);
+  if (Number.isNaN(totalRound)) {
+    fail("이동 횟수는 숫자만 입력 가능합니다.");
+  }
+  if (!Number.isInteger(totalRound) || totalRound <= 0) {
+    fail("이동 횟수는 양수인 정수만 입력 가능합니다.");
+  }
+  return totalRound;
 }
 
 class App {
