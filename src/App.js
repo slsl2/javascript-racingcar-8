d0e9a992faa4b64b2497c5ff1a71a8a8b5f4e34c
slsl2 <kimminji7153@gmail.com>
2025-10-27 05:47:06 +0900

    feat(app): 경주할 자동차 이름 입력, 이동 수 입력 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+// 이름 입력 받는 함수
+async function inputCarNames() {
+  const NAMES_INPUT = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  return NAMES_INPUT;
+}
+
+// 이동 횟수 입력 받는 함수
+async function inputTotalRound() {
+  const TOTAL_ROUND_INPUT = await Console.readLineAsync(
+    "시도할 횟수는 몇 회인가요?\n"
+  );
+  return TOTAL_ROUND_INPUT;
+}
+
 class App {
   async run() {}
 }
