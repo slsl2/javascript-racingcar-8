ded922df5cc90ed7c648158358c246a33b86c573
slsl2 <kimminji7153@gmail.com>
2025-10-27 06:57:33 +0900

    feat(app): 자동차 이름 입력값 split하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-// 에러 발생시키는 함수
 function fail(msg) {
   throw new Error(`[ERROR] ${msg}`);
 }
 
-// 이름 입력 받는 함수
 async function inputCarNames() {
   const namesInput = await Console.readLineAsync(
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
@@ -13,7 +11,6 @@ async function inputCarNames() {
   return namesInput;
 }
 
-// 이동 횟수 입력 받는 함수
 async function inputTotalRound() {
   const totalRoundInput = await Console.readLineAsync(
     "시도할 횟수는 몇 회인가요?\n"
@@ -21,23 +18,12 @@ async function inputTotalRound() {
   return totalRoundInput;
 }
 
-// 이름 유효성 검사
 function validateCarName(name) {
-  if (name < 5) {
+  if (name.lenght > 5) {
     fail("자동차 이름은 5자 이하만 가능합니다.");
   }
 }
 
-// 무작위 값 구해서 4 이상일 경우 전진 (true)
-function canMove() {
-  const randomNumber = Random.pickNumberInRange(0, 9);
-  if (randomNumber >= 4) {
-    return true;
-  }
-  return false;
-}
-
-// 이동 횟수 입력값 유효성 검사
 function validateTotalRound(totalRoundInput) {
   if (totalRoundInput === "") {
     fail("이동 횟수가 입력되지 않았습니다.");
@@ -50,6 +36,15 @@ function validateTotalRound(totalRoundInput) {
     fail("이동 횟수는 양수인 정수만 입력 가능합니다.");
   }
   return totalRound;
+}
+
+function splitCarNames(namesInput) {
+  const names = namesInput
+    .split(",")
+    .map((name) => name.trim())
+    .filter((name) => name !== "");
+  names.forEach(validateCarName);
+  return names;
 }
 
 class App {
