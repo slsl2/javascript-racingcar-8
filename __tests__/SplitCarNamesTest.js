aa461d157f728be0e63d6ac7c2186d29a6065001
slsl2 <kimminji7153@gmail.com>
2025-10-27 09:32:21 +0900

    test(input): splitCarNames 함수 테스트 코드 작성

---
+++ __tests__/SplitCarNamesTest.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@jest/globals';
+import { splitCarNames } from '../src/input/SplitCarNames';
+
+test('문자열-배열 변환 테스트', () => {
+  const result = splitCarNames('tobi, wooki, podol');
+  expect(result).toEqual(['tobi', 'wooki', 'podol']);
+});
+
+test('문자열-배열 변환, 공백 제거 테스트', () => {
+  const result = splitCarNames(' tobi ,wooki,   podol ');
+  expect(result).toEqual(['tobi', 'wooki', 'podol']);
+});
