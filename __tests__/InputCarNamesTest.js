0111bd18207efbf757735615bb6566db1c8039cb
slsl2 <kimminji7153@gmail.com>
2025-10-27 09:40:16 +0900

    test: inputCarNames 함수 테스트 코드 작성

---
+++ __tests__/InputCarNamesTest.js
@@ -0,0 +1,27 @@
+import { describe, test, expect, jest } from '@jest/globals';
+import { Console } from '@woowacourse/mission-utils';
+import { inputCarNames } from '../src/input/InputCarNames';
+
+describe('inputCarNames', () => {
+  test('Console.readLineAsync가 호출되는지 테스트', async () => {
+    const mockReadLine = jest
+      .spyOn(Console, 'readLineAsync')
+      .mockResolvedValue('tobi ,wooki, podol');
+
+    await inputCarNames();
+
+    expect(mockReadLine).toHaveBeenCalledTimes(1);
+    expect(mockReadLine).toHaveBeenCalledWith(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+
+    mockReadLine.mockRestore();
+  });
+
+  test('입력값이 그대로 반환되는지 테스트', async () => {
+    jest.spyOn(Console, 'readLineAsync').mockResolvedValue('tobi ,wooki, podol');
+
+    const result = await inputCarNames();
+    expect(result).toBe('tobi ,wooki, podol');
+  });
+});
