66a445fb941262c87b4decbd80029824377c0e93
slsl2 <kimminji7153@gmail.com>
2025-10-27 09:30:18 +0900

    test(race): printWinners 함수 테스트 코드 작성

---
+++ __tests__/PrintWinnersTest.js
@@ -0,0 +1,24 @@
+import { describe, test, expect, jest } from '@jest/globals';
+import { Console } from '@woowacourse/mission-utils';
+import { printWinners } from '../src/race/PrintWinners';
+
+jest.mock('@woowacourse/mission-utils', () => ({
+  Console: { print: jest.fn() },
+}));
+
+describe('printRound', () => {
+  test('최종 우승자 출력 테스트', () => {
+    // given
+    const cars = [
+      { name: 'tobi', pos: 4 },
+      { name: 'wooki', pos: 3 },
+      { name: 'podol', pos: 5 },
+    ];
+
+    // when
+    printWinners(cars);
+
+    // then
+    expect(Console.print).toHaveBeenNthCalledWith(1, '최종 우승자 : podol');
+  });
+});
