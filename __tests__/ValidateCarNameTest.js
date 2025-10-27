2f305a131e4ee2ebbf6d4e4e7cf1167b9fe45a83
slsl2 <kimminji7153@gmail.com>
2025-10-27 10:33:58 +0900

    test(utils) validateCarName 함수 테스트 코드 작성

---
+++ __tests__/ValidateCarNameTest.js
@@ -0,0 +1,23 @@
+import { beforeEach, describe, test, expect, jest } from '@jest/globals';
+import { validateCarName } from '../src/utils/ValidateCarName';
+import { fail } from '../src/utils/Fail';
+
+jest.mock('../src/utils/Fail', () => ({
+  fail: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('validateCarName', () => {
+  test('5자 초과 시에 fail 호출되는지 테스트', () => {
+    validateCarName('teletobi');
+    expect(fail).toHaveBeenCalledWith('자동차 이름은 5자 이하만 가능합니다.');
+  });
+
+  test('5자 이하일 경우에 fail 호출 안 되는지 테스트', () => {
+    validateCarName('tobi');
+    expect(fail).not.toHaveBeenCalled();
+  });
+});
